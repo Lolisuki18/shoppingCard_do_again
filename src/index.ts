de7a0b3e83ec74e6e4cdb92f1f19f9838129c267
Lolisuki18 <leninh2004@gmail.com>
2024-12-15 15:36:05 +0700

    done-setup-nodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello')
+import { log } from 'console'
+import express from 'express' // import express vào dự á
+
+const app = express() // dùng express tạo 1 server
+const port = 3000 // server sẽ chạy trên cổng port 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Project này đang chạy trên post ${port}`)
+})
